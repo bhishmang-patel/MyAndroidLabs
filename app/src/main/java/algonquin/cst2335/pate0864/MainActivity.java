1bddc2806091d65094ed13a8c44ebb02f7b1f222
Bhishmang Patel <pate0864@algonquinlive.com>
2021-10-11 13:26:11 -0400

    Finished Week_4

--- app/src/main/java/algonquin/cst2335/pate0864/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/pate0864/MainActivity.java
@@ -2,37 +2,71 @@ package algonquin.cst2335.pate0864;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
-import android.widget.Switch;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.BreakIterator;
 
-        @Override
+public class MainActivity<clk> extends AppCompatActivity {
+
+
+    private static String TAG = "MainActivity";
+    @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
 
-            ImageView imgView = findViewById(R.id.flagview);
-            Switch sw = findViewById(R.id.spin_switch);
+        EditText edit_Text= findViewById(R.id.editTextEmailAddress3);
+        Button loginButton = findViewById(R.id.loginButton);
 
-            sw.setOnCheckedChangeListener( (btn, isChecked) -> {
-                if (isChecked)
-                {
-                    RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                    rotate.setDuration(5000);
-                    rotate.setRepeatCount(Animation.INFINITE);
-                    rotate.setInterpolator(new LinearInterpolator());
-
-                    imgView.startAnimation(rotate);
-                }
-                else {
-                    imgView.clearAnimation();
-                }
-
-            });
+        loginButton.setOnClickListener(clk -> {
+            String emailEditText = edit_Text.getText().toString();
+            Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
+            nextPage.putExtra( "EmailAddress", emailEditText);
+            nextPage.putExtra("Age", 21);
+            nextPage.putExtra("Name", "Bhishmang");
+            nextPage.putExtra("PostalCode", "K2B7S9");
+            startActivity( nextPage);
+        });
         }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w(TAG, "Waring Message");
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w(TAG, "Waring Message");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.w(TAG, "Waring Message");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w(TAG, "Waring Message");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w(TAG, "Waring Message");
+    }
+}
---
+++ app/src/main/java/algonquin/cst2335/pate0864/SecondActivity.java
@@ -0,0 +1,122 @@
+package algonquin.cst2335.pate0864;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+    ImageView profileImage;
+    ActivityResultLauncher <Intent> cameraResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+
+                @Override
+
+                public void onActivityResult(ActivityResult result) {
+
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+
+                        Intent data = result.getData();
+                        File whereIam = getFilesDir();
+                        Bitmap thumbnail = data.getParcelableExtra("data");
+
+                        try {
+
+                            FileOutputStream fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+                            thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                            fOut.flush();
+                            fOut.close();
+
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        catch(IOException ioe){
+                            Log.w("IOException", "Can't get PNG");
+                        }
+
+                        Log.i("Got bitmap", "image");
+                        profileImage.setImageBitmap( thumbnail );
+                    }
+                    else if (result.getResultCode() == Activity.RESULT_CANCELED)
+                        Log.w("Got bitmap", "User refused the image");
+
+                }
+
+            });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent fromPrevious = getIntent();
+        String emailAddress = fromPrevious.getStringExtra("EmailAddress");
+        int age = fromPrevious.getIntExtra("Age", 0);
+        String name = fromPrevious.getStringExtra("Name");
+        TextView wlc_msg = findViewById(R.id.textView3);
+        wlc_msg.setText("Welcome Back" + " " + emailAddress);
+        String pCode = fromPrevious.getStringExtra("PostalCode");
+
+        //Shared Preferences
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        prefs.getString("LoginName", emailAddress);
+        prefs.getInt("Age", age);
+        prefs.getString("Name", name);
+        prefs.getString("Postalcode", pCode);
+        String edit_Text = prefs.getString("LoginName", "");
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("LoginName", emailAddress);
+        editor.putInt("Age", age);
+        editor.putString("Name", name);
+        editor.putString("PostalCode", pCode);
+        editor.apply();
+
+        // Click listener method
+        EditText number = findViewById(R.id.editTextPhone);
+        Button dialer = findViewById(R.id.button2);
+
+        dialer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent call = new Intent(Intent.ACTION_DIAL);
+                call.setData(Uri.parse("tel:"+number.getText().toString()));
+                startActivity(call);
+            }
+        });
+
+        ImageView img = findViewById(R.id.imageView);
+        img.setOnClickListener(clk -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            cameraResult.launch(cameraIntent);
+        });
+        Button change_pic = findViewById(R.id.button);
+        change_pic.setOnClickListener(clk -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            cameraResult.launch(cameraIntent);
+
+        });
+    }
+}
